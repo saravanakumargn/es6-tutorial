6485afdc691192e22657db30d9247f39dc12b44e
Saravanakumar <saravanakumargn@gmail.com>
2016-11-24 09:42:15 +0530

    pagestyle and layout update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
   function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'https://saravanakumargn.github.io/es6-tutorial/assets/js/menu.json', true);
+    xobj.open('GET', 'http://localhost:3000/assets/js/menu.json', true);
     xobj.onreadystatechange = function () {
       if (xobj.readyState == 4 && xobj.status == "200") {
         callback(xobj.responseText);
@@ -13,17 +13,12 @@
   function init() {
     loadJSON(function (response) {
       var menu = JSON.parse(response).menu;
-      var linksMenu = '', links = '<ul class="toc">';
+      var link = '';
       var menuLen = menu.length;
       for (var i = 0; i < menuLen; i++) {
-        linksMenu += '<a class="mdl-navigation__link" href="' + menu[i].url + '">' + menu[i].label + '</a>';
-        links += '<a href="' + menu[i].url + '">' + menu[i].label + '</a>';
+        link += '<a class="mdl-navigation__link" href="' + menu[i].url + '">' + menu[i].label + '</a>';
       }
-      links += '</ul>';
-      document.getElementById('pageMenu').innerHTML = linksMenu;
-      if (document.getElementById('pageMenu1')) {
-        document.getElementById('pageMenu1').innerHTML = links;
-      }
+      document.getElementById('pageMenu').innerHTML = link;
     });
   }
   init();
